7447d094553ad1e35626eafffdfa905759adf276
Aisyazhs <aisyahazizahs2000@gmail.com>
2022-10-29 22:40:57 +0700

    first commit

---
+++ src/main/java/ws/a/hiling/HilController.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ws.a.hiling;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ *
+ * @author AISYAH SUHERMAN
+ */
+@Controller
+public class HilController {
+    }
---
+++ src/test/java/ws/a/hiling/HilingApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.hiling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
