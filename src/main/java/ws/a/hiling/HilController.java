a7070076e18b88de7dde19913c73f7290e265365
Aisyazhs <aisyahazizahs2000@gmail.com>
2022-10-31 00:31:50 +0700

    Add Index, View, and Controller

--- src/main/java/ws/a/hiling/HilController.java
+++ src/main/java/ws/a/hiling/HilController.java
@@ -4,11 +4,14 @@
  */
 package ws.a.hiling;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -16,4 +19,27 @@ import org.springframework.web.bind.annotation.ResponseBody;
  */
 @Controller
 public class HilController {
+    @RequestMapping("/view")
+    //@ResponseBody
+    public String tampil(
+            @RequestParam(value = "nama") String nama,
+            @RequestParam(value = "lokasi") String lokasi,
+            @RequestParam(value = "gambar") MultipartFile gambar,
+            Model model
+            
+    ) throws IOException{  
+        byte[] img = gambar.getBytes();
+        String base64Image = Base64.encodeBase64String(img);
+        String pic= "data:image/png;base64,".concat(base64Image);
+      
+        
+        
+        model.addAttribute("nama", nama);
+        model.addAttribute("lokasi", lokasi);
+        model.addAttribute("gmb", pic);
+        
+        return "view";
+    
+    
     }
+}
